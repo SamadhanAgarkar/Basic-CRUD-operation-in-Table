80d8ff3cd86917f35a3353efd6edb144333afc7e
unknown <sagarkar088@gmail.com>
2023-12-22 01:34:49 +0530

    basic cruid operations are done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CRUDComponent from "./component/CRUDComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <CRUDComponent />
+    </div>
+  );
+}
+
+export default App;
