df840d1ff76685f4894b0b817ca43e2ea47ce9bb
unknown <sagarkar088@gmail.com>
2024-01-03 10:17:03 +0530

    input box padding updated as per design

--- src/component/CRUDComponent.js
+++ src/component/CRUDComponent.js
@@ -65,7 +65,7 @@ const CRUDComponent = () => {
                 name="name"
                 value={inputs.name}
                 onChange={handleChange}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6"
               />
             </div>
             <br></br>
@@ -78,7 +78,7 @@ const CRUDComponent = () => {
               </label>
               <input
                 name="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6"
                 value={inputs.email}
                 onChange={handleChange}
               />
